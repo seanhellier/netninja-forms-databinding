20ac1dec464c33c920070587efa8eb3f7884355e
Sean Leif Hellier <sean@newtropolis.com>
2021-06-25 18:07:40 -0400

    lesson 42

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,39 +1,58 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required v-model="email">
-        <label>Password:</label>
-    <input type="password" required v-model="password">
-  </form>
+    <input type="email" v-model="email" required>
 
-    <Label>Role: </Label>
+    <label>Password:</label>
+    <input type="password" v-model="password" required>
+
+    <label>Role:</label>
     <select v-model="role">
-        <option value="developer">Web Developer</option>
-        <option value="designer">Web designer</option>
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
     </select>
 
-    <div class="terms">
-        <input type="checkbox" v-model="terms" required>
-        <label>Accept terms and conditions</label>
+    <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
     </div>
 
-    <p>Email {{ email }}</p>
-    <p>Password {{ password }}</p>
-    <p>Role {{ role }}</p>
-    <p>Terms  {{ terms }}</p>  
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+  </form>
 
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Your role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            email: ``,
-            password: ``,
-            role: ``,
-            terms: `false`
-        }
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'developer',
+      terms: false,
+      skills: [],
+      tempSkill: '',
     }
+  },
+  methods: {
+    addSkill($event) {
+      console.log($event)
+      if($event.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill)
+        }
+        this.tempSkill = ''
+      }
+    },
+  }
 }
 </script>
 
